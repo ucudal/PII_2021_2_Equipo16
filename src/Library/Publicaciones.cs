b98bf73f037597596a35503d94ca9d9b45c56b34
PieroSaucedo <pierosaucedo@correo.ucu.edu.uy>
2021-10-27 20:59:10 -0300

    Más cambios en la clase Publicaciones

--- src/Library/Publicaciones.cs
+++ src/Library/Publicaciones.cs
@@ -3,24 +3,12 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace ClassLibrary
-{
-    public static class Publicaciones 
+{ 
+    public class Publicaciones 
     {
-        
-        public static List<Oferta> OfertasPublicados = new List<Oferta>();
+        public List<Oferta> OfertasPublicados = new List<Oferta>();
         // Por Creator 
-        public static void AddOferta(string nombre, string material, int precio, string unidad, int cantidad, string tags, string ubicacion)
-        {
-            Oferta oferta = new Oferta(nombre, material, precio, unidad, cantidad, tags, ubicacion);
-            OfertasPublicados.Add(Oferta);
-        }
-        // Por Creator
-        public static void RemoveOferta(string nombre, string material, int precio, string unidad, int cantidad, string tags, string ubicacion)
-        {
-            Oferta oferta = new Oferta(nombre, material, precio, unidad, cantidad, tags, ubicacion);
-            OfertasPublicados.Remove(oferta);
-        }
-        public static void GetOfertasPublicados()
+        public void GetOfertasPublicados()
         {
             StringBuilder getOfertasPublicados = new StringBuilder("Ofertas: \n");
             foreach (Oferta oferta in OfertasPublicados)
@@ -28,14 +16,6 @@ namespace ClassLibrary
                 getOfertasPublicados.Append($"- {oferta.Nombre}.");   
             }
             Console.WriteLine(getOfertasPublicados.ToString());
-        }
-        public static Oferta GetOferta(Oferta oferta)
-        {
-            OfertasPublicados.Remove(oferta);
-            return oferta;
-        }
-
-
-        
+        }        
     }
 }
