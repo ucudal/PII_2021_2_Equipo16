ce3493a6264532aa0a40d66c1e89352b8c89317f
PieroSaucedo <piero.saucedo@correo.ucu.edu.uy>
2021-10-31 17:44:13 -0300

    ConsolePrinter añadida y IPrinter añadida

---
+++ src/Library/IPrinter.cs
@@ -0,0 +1,13 @@
+namespace ClassLibrary
+{
+    /// <summary>
+    /// Esta interfaz contiene un método para imprimir.
+    /// </summary>
+    public interface IPrinter
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        void ofertaPrinter (Oferta oferta);
+    }
+}
