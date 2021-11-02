d225789a386b764be3fc82dbe322c41b4674bbd4
RowletGA <joaquin.perez@correo.ucu.edu.uy>
2021-11-02 16:31:49 -0300

    Xml arreglados

--- src/Library/BuscadorTags.cs
+++ src/Library/BuscadorTags.cs
@@ -13,9 +13,9 @@ namespace ClassLibrary
         /// <summary>
         /// Busca ofertas en Publicaciones, según los tags de la oferta.
         /// </summary>
-        /// <param name="publicaciones">Recibe un parametro de tipo Publicaciones con el nombre de "publicaciones"</param>
-        /// <param name="busqueda">Recibe un parametro de tipo string con el nombre de "busqueda"</param>
-        /// <returns>Retorna las ofertas encontradas por tag, mediante una lista de tipo Oferta</returns>
+        /// <param name="publicaciones"> Recibe un parametro de tipo Publicaciones con el nombre de "publicaciones".</param>
+        /// <param name="busqueda"> Recibe un parametro de tipo string con el nombre de "busqueda".</param>
+        /// <returns> Retorna las ofertas encontradas por tag, mediante una lista de tipo Oferta.</returns>
         public List<Oferta> Buscar(Publicaciones publicaciones, string busqueda)
         {   List<Oferta> ofertasEncontradas = new List<Oferta>();
             StringBuilder busquedaTagtxt = new StringBuilder();
@@ -33,7 +33,7 @@ namespace ClassLibrary
             return ofertasEncontradas;
 
         }
-    }   // Fin de la clase BuscadorTags 
+    }   // Fin de la clase BuscadorTags.
     
     
 }
