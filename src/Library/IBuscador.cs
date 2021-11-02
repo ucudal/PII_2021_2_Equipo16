fbe0058a4072c80a489ccd6e34da89d15e40902c
RowletGA <joaquin.perez@correo.ucu.edu.uy>
2021-11-02 16:32:59 -0300

    xml arreglos

--- src/Library/IBuscador.cs
+++ src/Library/IBuscador.cs
@@ -6,16 +6,16 @@ using System.Text;
 namespace ClassLibrary
 {
      /// <summary>
-     /// Interfaz IBuscador que define el metodo a implementar por las clases de busqueda
+     /// Interfaz IBuscador que define el metodo a implementar por las clases de busqueda.
      /// </summary>
    public interface IBuscador
     {
 
          /// <summary>
-         /// Interfaz IBuscador
+         /// Interfaz IBuscador.
          /// </summary>
-         /// <param name="publicaciones">Recibe parametro del tipo Publicaciones </param>
-         /// <param name="busqueda"> Recibe parametro del tipo string </param>
+         /// <param name="publicaciones">Recibe parametro del tipo Publicaciones. </param>
+         /// <param name="busqueda"> Recibe parametro del tipo string. </param>
          /// <returns></returns>
          List<Oferta> Buscar(Publicaciones publicaciones, string busqueda);
        
