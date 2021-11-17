b18867763961256458628b292f573cbf6cef93cf
PieroSaucedo <pierosaucedo@correo.ucu.edu.uy>
2021-11-17 18:42:11 -0300

    Nuevos Handlers y Clases modificadas

---
+++ src/Library/Handlers/AceptarInvEmpresaHandler.cs
@@ -0,0 +1,20 @@
+using Telegram.Bot.Types;
+
+namespace ClassLibrary
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class AceptarInvEmpresaHandler : BaseHandler
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="next"></param>
+        /// <returns></returns>
+        public AceptarInvEmpresaHandler (BaseHandler next) : base(next)
+        {
+            
+        }
+    }
+}
