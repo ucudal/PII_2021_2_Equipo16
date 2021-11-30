fe318e46ae5a11f9753bc1dc2f0c097480cdd1a0
Agustin09810 <a.lorenzogon2@gmail.com>
2021-11-30 17:29:46 -0300

    Tests EmprendedorHandlers

--- test/LibraryTests/RubroTests.cs
+++ test/LibraryTests/RubroTests.cs
@@ -18,14 +18,5 @@ namespace Test.Library
             Rubro rubro = new Rubro("Efectivamente");
             Assert.That(rubro.Nombre, Is.EqualTo("Efectivamente"));
         }
-        /// <summary>
-        /// En este test vamos a verificar la correcta creación de un Rubro sin nombre
-        /// </summary>
-        [Test]
-        public void TestCrearRubro2()
-        {
-            Rubro rubro = new Rubro();
-            Assert.That(rubro.Nombre, Is.EqualTo(null));
-        }
     }
 }
